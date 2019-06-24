6f7e6a4df6b738d957a18c9ad8ef4375f9493b5f
logomanwolf <1293789686@qq.com>
2019-06-24 20:59:39 +0800

    init server and add BIGCLAM

---
+++ utils/community/utils/pre_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import networkx as nx  
+groundTruth="E:/Download/social_network/email-Eu-core-department-labels.txt/email-Eu-core-department-labels.txt"
+edgeList="E:/Download/social_network/email-Eu-core.txt/email-Eu-core.txt"
+def preData():
+    
+    G=nx.read_edgelist(edgeList)
+    size=len(nx.nodes(G))
+    edges=G.edges()
+    # labels=map(change,np.loadtxt(directory+edgeList))
+    # for line in nx.generate_edgelist(G)
+    new_array=np.zeros((size,size),dtype=int)
+    for line in edges:
+        new_array[int(line[0])][int(line[1])]=1
+        new_array[int(line[1])][int(line[0])]=1
+    return new_array 
+def preLabel():
+    pass
+if __name__ == "__main__":
+    preData()

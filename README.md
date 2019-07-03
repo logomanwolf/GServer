92b5ae59ec022b821b8679878d611e83bd8dcfea
logomanwolf <1293789686@qq.com>
2019-07-03 10:57:32 +0800

    1. 增加了usage 2. 增加了api 接受post请求

--- README.md
+++ README.md
@@ -8,6 +8,34 @@ The back-end for big-graph demo.
 ## Start
 `nodemon app.js`
 ## Usage
-`localhost:3000/communityDetect    //detect community`
-` localhost:3000/pageRank            //page rank`
-` localhost:3000/shortestPath/1/2           //The shortest paths from node#1 to node#2.`
+### basic usage
+`localhost:3000/communityDetect                  //detect community`
+` localhost:3000/pageRank                         //page rank`
+` localhost:3000/shortestPath/1/2                 //The shortest paths from node#1 to node#2.`
+
+### api
+
+|                 |       参数格式        |                         输出                         |
+| :-------------: | :-------------------: | :--------------------------------------------------: |
+| communityDetect |           -           |    {0:1,1:1}  //string , 反映每个节点属于哪个社团    |
+|    pageRank     |           -           |     {0:0.01,1:0.02}//string, 反映每个节点的权值      |
+|  shortestPath   | {start：int, end:int} | [[‘1’,’3’,’2’],[‘1’,’4’,’5’,’2’]] //string, 最短路径 |
+
+## Example
+
+```javascript
+var params={start:1,end:2}
+$.ajax({
+    url:"localhost:3000/shortestPath",
+    type:"POST",
+    data:params,
+    dataType:String,
+    success:(data)=>{
+        console.log(data);
+    }
+})
+//[[‘1’,’3’,’2’],[‘1’,’4’,’5’,’2’]]
+```
+
+### 
+
--- app.js
+++ app.js
@@ -8,31 +8,58 @@ const app = new Koa();
 
 const call=require('./call.js')
 // const filename="E:/Download/social_network/Email-EuAll.txt/Email-EuAll.edgelist"
-const filename="./data/Email-EuAll.edgelist"
+const filename="./data/email-Eu-core.edgelist"
 // 对于任何请求，app将调用该同步函数处理请求：
+// 引入
+const bodyParser = require('koa-bodyparser')
+// 配置中间件
+app.use(bodyParser())
+// 调用路由中间件
+app.use(router.routes())
 //社团检测
+router.post('/communityDetect', async (ctx, next) => {
+    const result=call.communityDetect(filename)
+    ctx.response.body=result
+})
 router.get('/communityDetect', async (ctx, next) => {
     const result=call.communityDetect(filename)
     ctx.response.body=result
 })
-
 //最短路径，需要输入参数(起始节点)
-router.get('/shortestPath/:start/:end',async(ctx,next)=>{
-    const {start,end}=ctx.params
+router.post('/shortestPath',async(ctx,next)=>{
+    console.log(ctx.request.body)
+    const {start,end}=ctx.request.body
     const argvs=filename+' '+start+' '+end+' '
     console.log(argvs)
     const result=call.shortestPath(argvs) 
     ctx.response.body=result
 })
-
+router.get('/shortestPath',async(ctx,next)=>{
+    console.log(ctx.request.body)
+    const {start,end}=ctx.request.body
+    const argvs=filename+' '+start+' '+end+' '
+    console.log(argvs)
+    const result=call.shortestPath(argvs) 
+    ctx.response.body=result
+})
+// router.get('/shortestPath',async(ctx,next)=>{
+//     console.log(ctx.query)
+//     const {start,end}=ctx.query
+//     const argvs=filename+' '+start+' '+end+' '
+//     console.log(argvs)
+//     const result=call.shortestPath(argvs) 
+//     ctx.response.body=result
+// })
 //节点排名，返回每一个节点的排名指数，所有节点排名指数相加等于1
+router.post('/pageRank', async (ctx, next) => {
+    const result=call.pageRank(filename)
+    ctx.response.body=result
+})
 router.get('/pageRank', async (ctx, next) => {
     const result=call.pageRank(filename)
     ctx.response.body=result
 })
 
-// 调用路由中间件
-app.use(router.routes())
 // 在端口3000监听:
 app.listen(3000);
 console.log('app started at port 3000...');
